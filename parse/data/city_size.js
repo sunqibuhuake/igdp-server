c5bac27ba023fd68cce176fd9407e320d175dace
没有bug的无聊世界 <12824031@qq.com>
2018-07-11 19:16:32 +0800

    2

---
+++ parse/data/city_size.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        id: 1,
+        name: 'Mega'
+    }, 
+    {
+        id: 2,
+        name: 'Very Large'
+    },
+    {
+        id: 3,
+        name: 'Large'
+    },
+    {
+        id: 4,
+        name: 'Medium + Small'
+    }
+]
