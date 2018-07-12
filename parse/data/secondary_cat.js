b58369ef27c2cb6df4567044a51a0404a4889965
没有bug的无聊世界 <12824031@qq.com>
2018-07-12 11:20:51 +0800

    1

---
+++ parse/data/secondary_cat.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+        id: 1,
+        en: 'Economic Dimension',
+        zh: ''
+    },
+    {
+        id: 2,
+        en: 'Energy & Power',
+        zh: ''
+    },
+    {
+        id: 3,
+        en: 'Industry',
+        zh: ''
+    },
+    {
+        id: 4,
+        en: 'Transportation',
+        zh: ''
+    },
+    {
+        id: 5,
+        en: 'Buildings',
+        zh: ''
+    },
+    {
+        id: 6,
+        en: 'Environment & Land Use',
+        zh: ''
+    },
+    {
+        id: 7,
+        en: 'Policy Dimension',
+        zh: ''
+    }
+]
+
+module.exports = data;
+export default data; 
---
+++ parse/raw/indicators.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const result = [];
+fs.readFile('./indicators.raw', (err, data) => {
+    const lines = data.split('\n');
+    lines.forEach(line => {
+        const arr = line.split('#');
+        if (arr.length > 1) {
+            result.push({
+                id: arr[0],
+                name: arr[1],
+                unit: arr[2],
+                scat: arr[3],
+                max: arr[4],
+                label: arr[5]
+            })
+        }
+    })
+
+    fs.writeFile('./indicators.json',JSON.stringify({data: result}), 'utf-8', (err) => {
+        err && console.log(err)
+        console.log('done')
+    })
+})
